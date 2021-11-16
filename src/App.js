3938f22051a894d354c5c24415d18f11d250e710
Kevin Torres <kevin_akd@hotmail.com>
2021-11-16 18:29:32 -0500

    Juego funcional

--- src/App.js
+++ src/App.js
@@ -5,20 +5,45 @@ import { shuffle } from "lodash";
 
 function App() {
   const [cards, setCards] = useState(shuffle([...images, ...images]));
+  const [activeCards, setActiveCards] = useState([]);
+  const [matches, setMatches] = useState([]);
+
+  function flipCard(index) {
+    if (activeCards.length === 0) setActiveCards([index]);
+    if (activeCards.length === 1) {
+      const firstIndex = activeCards[0];
+      const secondIndex = index;
+      if (cards[firstIndex] === cards[secondIndex]) {
+        setMatches([...matches, firstIndex, secondIndex]);
+      }
+      setActiveCards([...activeCards, index]);
+    }
+    if (activeCards.length === 2) {
+      setActiveCards([index]);
+    }
+  }
 
   return (
     <div className="App">
       <div className="board">
-        {cards.map((card, index) => (
-          <div className="card-outer">
-            <div className="card">
-              <div className="front">
-                <img src={card} alt=""></img>
+        {cards.map((card, index) => {
+          const flipped =
+            activeCards.indexOf(index) !== -1 || matches.indexOf(index) !== -1;
+          return (
+            <div
+              className={"card-outer " + (flipped ? "flipped" : "")}
+              key={index}
+              onClick={() => flipCard(index)}
+            >
+              <div className="card">
+                <div className="front">
+                  <img src={card} alt=""></img>
+                </div>
+                <div className="back"></div>
               </div>
-              <div className="back"></div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </div>
   );
