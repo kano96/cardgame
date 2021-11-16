2f7746d36393a467b1ce26b9ab62908b95bab781
Kevin Torres <kevin_akd@hotmail.com>
2021-11-16 17:09:48 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
+import images from "./Images";
 
 function App() {
-  return <div className="App"></div>;
+  const [cards, setCards] = useState([...images, ...images]);
+
+  return (
+    <div className="App">
+      <div className="board">
+        {cards.map((card, index) => (
+          <div className="card-outer">
+            <div className="card">
+              <div className="front">
+                <img src={card} alt=""></img>
+              </div>
+              <div className="back"></div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Images.js
@@ -0,0 +1,13 @@
+import Img1 from "./heros/01.png";
+import Img2 from "./heros/02.png";
+import Img3 from "./heros/03.png";
+import Img4 from "./heros/04.png";
+import Img5 from "./heros/05.png";
+import Img6 from "./heros/06.png";
+import Img7 from "./heros/07.png";
+import Img8 from "./heros/08.png";
+import Img9 from "./heros/09.png";
+import Img10 from "./heros/10.png";
+
+const images = [Img1, Img2, Img3, Img4, Img5, Img6, Img7, Img8, Img9, Img10];
+export default images;
